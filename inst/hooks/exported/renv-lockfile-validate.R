50489aeb4e9a22ba1ffaef5eefb6f879cccebf97
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2024-12-13 08:48:56 -0500

    Re-add schema

--- inst/hooks/exported/renv-lockfile-validate.R
+++ inst/hooks/exported/renv-lockfile-validate.R
@@ -5,36 +5,32 @@
 See `?renv::lockfile_validate()`.
 
 Usage:
-  lockfile_validate [--greedy --error --verbose --strict] <files>...
+  lockfile_validate [--schema=<schema>] [--greedy --error --verbose --strict] <files>...
 
 Options:
-  --greedy  Boolean. Continue after first error?
-  --error  Boolean. Throw an error on parse failure?
-  --verbose  Boolean. If `TRUE`, then an attribute `errors` will list validation failures as a `data.frame`.
-  --strict  Boolean. Set whether the schema should be parsed strictly or not.
+  --schema Path. Path to a custom schema.
+  --greedy  Continue after first error?
+  --error  Throw an error on parse failure?
+  --verbose  If `TRUE`, then an attribute `errors` will list validation failures as a `data.frame`.
+  --strict  Set whether the schema should be parsed strictly or not.
 " -> doc
+
 arguments <- precommit::precommit_docopt(doc)
-print(arguments)
 arguments$files <- normalizePath(arguments$files)
-print(arguments)
+
 if (!require(renv, quietly = TRUE)) {
   stop("{renv} could not be loaded, please install it.")
 }
 if (packageVersion("renv") < package_version("1.0.8")) {
   rlang::abort("You need at least version 1.0.8 of {renv} to run this hook.")
 }
-
 if (!require(jsonvalidate, quietly = TRUE)) {
   stop("{jsonvalidate} could not be loaded, please install it.")
 }
 
-
-print(arguments)
-print(arguments$greedy)
-print(arguments$error)
-
 renv::lockfile_validate(
   lockfile = arguments$files,
+  schema = arguments$schema,
   greedy = arguments$greedy,
   error = arguments$error,
   verbose = arguments$verbose,
